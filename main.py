9030a16fc453b9de88285b2f11d55e37755e6655
quasar529 <46216280+quasar529@users.noreply.github.com>
2020-05-06 18:50:37 +0900

    github page

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
 from flask import Flask,render_template, redirect
-#from rocket import get_rp_jobs
+from rocket import get_rp_jobs
 from wanted import get_wanted_jobs
 from saramin import get_saramin_jobs
 from save import save_to_file
 #wanted_jobs=get_wanted_jobs("산업기능요원")
-saramin_jobs=get_saramin_jobs("산업기능요원")
-save_to_file(saramin_jobs)
-# driver=webdriver.Chrome('C:\\Users\\김범준\\Desktop\\Git\\WebScaping\\ITJobs\\WebScraper_ITJobs\\chromedriver.exe')
-# driver.implicitly_wait(5)
-# driver.get('https://www.rocketpunch.com/jobs?keywords=산업기능요원')
-# driver.implicitly_wait(5)
+#saramin_jobs=get_saramin_jobs("산업기능요원")
+#rocketpunch_jobs=get_rp_jobs("산업기능요원")
+#print(rocketpunch_jobs)
+app=Flask('esc_scraper')
+@app.route('/quasar529.github.io/WebScraper_ITJobs/')
+def home():
+    return render_template('home.html')
 
-# html=driver.page_source
-# soup=BeautifulSoup(html)
-
-# items=soup.select('.job-detail')
-# print(items)
-#pagination=soup.select('.pagination')
-#last_page=pagination[0]
-#for p in pagination:
-    #print(p.a.attrs['page'])
-#driver.close()
-# jobs=[]
-# for item in items:
-#     title=item.select_one('a').text
-#     link='http://www.saramin.co.kr/'+item.select_one('a').get('href')
-#     job={
-#         'title':title,
-#         'link':link
-#     }
-#     jobs.append(job)
+app.run()
--- rocket.py
+++ rocket.py
@@ -3,29 +3,56 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from urllib.parse import quote_plus
 # def extract_last_page(url):
-#     result=requests.get(url)
-#     soup=BeautifulSoup(result.text,"html.parser")
-#     pages=soup.find("div",{"class":"tablet computer large screen widescreen only"}).find_all("a")
-#     print(pages)
-#     last_page=pages[-1].string
-#     return last_page
-driver=webdriver.Chrome('C:\\Users\\김범준\\Desktop\\Git\\WebScaping\\ITJobs\\WebScraper_ITJobs\\chromedriver.exe')
-driver.implicitly_wait(5)
+#     driver=webdriver.Chrome('C:\\Users\\김범준\\Desktop\\Git\\WebScaping\\ITJobs\\WebScraper_ITJobs\\chromedriver.exe')
+#     driver.implicitly_wait(5)
+#     driver.get(url)
+#     driver.implicitly_wait(5)
+#     html=driver.page_source
+#     soup=BeautifulSoup(html,"html.parser")
+#     pagination=soup.select('.pagination')
+#     last_page=pagination[0]
+#     for p in pagination:
+#         print(p.a.attrs['page'])
+#     return 
+
 def extract_job(url):
-    
+    jobs=[]
+    driver=webdriver.Chrome('C:\\Users\\김범준\\Desktop\\Git\\WebScaping\\ITJobs\\WebScraper_ITJobs\\chromedriver.exe')
+    driver.implicitly_wait(5)
     driver.get(url)
     driver.implicitly_wait(5)
     html=driver.page_source
     soup=BeautifulSoup(html,"html.parser")
-    #print(soup)
-    #items=soup.find_all('div',{'class':'job-detail'})
-    items=soup.select(".ui job items segment")
-    print(items)
-    # for item in items:
-    #     #title=item.select('.job-detail')
-    #     print(item)
-    driver.close()
+    
+    items=soup.select('div.job-detail')
+    #names=soup.find_all('h4',{'class':'header name'})
+    # tmp=driver.find_element_by_xpath('//*[@id="company-list"]/div[3]/div[2]/div[2]/a[1]/h4/strong')
+    # driver.implicitly_wait(3)
+    # print(tmp)
+    # names=tmp.find_elements_by_tag_name('strong')
+    # driver.implicitly_wait(3)
+    #names=soup.select('div.content > div.company-name')
+    tmp=driver.find_element_by_xpath('//*[@id="company-list"]')
+    names=tmp.find_elements_by_tag_name('strong')
+    
+    #print(tmp.get_attribute('text'))
+    for n in names:
+        print(n.get_attribute('text'))
+    for item in items:
+
+        title=item.select_one('a').text
+        link='https://www.rocketpunch.com'+item.select_one('a').get('href')
+        #name=n
+        job={
+            'title':title,
+            #'name':n,
+            'link':link
+        }
+        jobs.append(job)
+    return jobs
+
 def get_rp_jobs(word):
+    #jobs=[]
     url=f"https://www.rocketpunch.com/jobs?keywords={word}"
     jobs=extract_job(url)
     return jobs    
